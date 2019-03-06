6bc677adbadff8b4411178469ead4830ed9ac291
Jemes Mondol <jemes888@gmail.com>
2019-03-06 14:08:58 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,63 @@
+function myFunction() {
+  var y = 5;
+  var x = y + 2;
+  document.getElementById("demo").innerHTML = x;
+}
+function myFunction() {
+  var y = 5;
+  var x = y - 2;
+  document.getElementById("substraction").innerHTML = x;
+}
+function myFunction() {
+  var y = 5;
+  var x = y * 2;
+  document.getElementById("multiplication").innerHTML = x;
+}
+function myFunction() {
+  var y = 5;
+  var x = y / 2;
+  document.getElementById("division").innerHTML = x;
+}
+function myFunction() {
+  var y = 5;
+  var x = y % 2;
+  document.getElementById("modulus").innerHTML = x;
+}
+function myFunction() {
+  var y = 5;
+  var x = ++y;
+  document.getElementById("increment1").innerHTML = y;
+  document.getElementById("increment2").innerHTML = x;
+}
+function myFunction() {
+  var y = 5;
+  var x = --y;
+  document.getElementById("decrement1").innerHTML = y;
+  document.getElementById("decrement2").innerHTML = x;
+}
+function myFunction() {
+  var x = 10;
+  var y = 5;
+  x = y;
+  document.getElementById("equal").innerHTML = x;
+}
+function myFunction() {
+  var x = 10;
+  var y = 5;
+  x += y;
+  document.getElementById("plusequal").innerHTML = x;
+}
+function myFunction() {
+  var text1 = "Good ";
+  var text2 = "Morning";
+  var text3 = text1 + text2;
+  document.getElementById("morning").innerHTML = text3;
+}
+function myFunction() {
+  var x = 5;
+  document.getElementById("equaltofalse").innerHTML = (x == 8);
+}
+function myFunction() {
+  var x = 5;
+  document.getElementById("equaltrue").innerHTML = (x == 5);
+}
